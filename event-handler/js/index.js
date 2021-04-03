9f817cb4db672d6f658cbcbcd22370d53af8ef64
Ng Chia Hui <ngchiahui95@gmail.com>
2021-04-02 19:00:56 -0700

    checkPassword

--- event-handler/js/index.js
+++ event-handler/js/index.js
@@ -6,3 +6,16 @@ let elNamaPengguna = document.getElementById("namaPengguna"); // 1)select elemen
 elNamaPengguna.addEventListener("keyup", function(){
 elNamaPengguna.value = transformUppercase(elNamaPengguna.value);
 });
+
+function check Password(){
+    let elKataLaluan = document.getElementById("kataLaluan");
+    let password_val = elKataLaluan.value;
+    If(password_val.length > 5){
+        alert("Password check...OK!");
+    } else {
+        alert("Password too short!");
+    }
+}
+
+let elKataLaluan = document.getElementById("KataLaluan");
+elKataLaluan.onblur = checkPassword;
