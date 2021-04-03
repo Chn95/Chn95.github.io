3091d69a556034fc255fd613402f58602631cd26
Ng Chia Hui <ngchiahui95@gmail.com>
2021-04-02 19:22:40 -0700

    edited

--- event-handler/js/index.js
+++ event-handler/js/index.js
@@ -7,10 +7,10 @@ elNamaPengguna.addEventListener("keyup", function(){
 elNamaPengguna.value = transformUppercase(elNamaPengguna.value);
 });
 
-function check Password(){
+function checkPassword(){
     let elKataLaluan = document.getElementById("kataLaluan");
     let password_val = elKataLaluan.value;
-    If(password_val.length > 5){
+    if(password_val.length > 5){
         alert("Password check...OK!");
     } else {
         alert("Password too short!");
