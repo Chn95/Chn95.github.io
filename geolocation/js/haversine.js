f04c8f0f5da2a82068d6bf5a02d408793053f370
Ng Chia Hui <ngchiahui95@gmail.com>
2021-04-05 17:23:59 -0700

    updated

--- geolocation/js/haversine.js
+++ geolocation/js/haversine.js
@@ -1,15 +1,12 @@
-function havershine(lat1,lon1,lat2,lon2){
+function havershine(lat1, lon1, lat2, lon2) {
     var R = 6371;// earth radius in KM
     var dLat = (lat2-lat1).toRad();
     var dLon = (lon2-lon1).toRad();
     var lat1 = lat1.toRad();
     var lat2 = lat2.toRad();
 
-    var a = Math.sin(dLat/2) *
-    Math.sin(dLat/2) + Math.sin(dLon/2) *
-    Math.sin(dLon/2) * Math.cos(lat1) *
-    Math.cos(lat2);
-    var c = 2 * Math.atan2(Math.sqrt(a),Math.sqrt(1-a));
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
     var d = R * c; // Kilometers
 
     return roundVal(d);
@@ -18,10 +15,9 @@ function havershine(lat1,lon1,lat2,lon2){
 //Converts numeric degrees to radians 
 
 if (typeof Number.prototype.toRad =='undefined'){
-    Number.prototype.toRad = function()
-{
+    Number.prototype.toRad = function(){
 return this * Math.PI / 180;
-}
+ }
 }
 
 function roundVal(val){
