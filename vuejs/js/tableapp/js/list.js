e2fc99548e14bfe4a4879156a717683bb615aaaa
Ng Chia Hui <ngchiahui95@gmail.com>
2021-04-09 20:56:25 -0700

    2148

--- vuejs/js/tableapp/js/list.js
+++ vuejs/js/tableapp/js/list.js
@@ -1,4 +1,35 @@
 let refreshNowBtn = document.getElementById("refreshNow");
 refreshNowBtn.addEventListener("click",function(){
-    alert("GetBooking");
+    GetBooking();
 });
+
+function GetBooking(){
+     let url = "https://api.sheety.co/";
+     fetch(url)
+     .then((response) => response.json())
+     .then(json => {
+
+        let bookingNameList = document.getElementById("bookingNameList");
+
+        //clear the table rows
+        for(let k = bookingNameList.rows.length - 1; k > 0; k--){
+            bookingNameList.deleteRow(k);
+        }
+
+        for(let i= 0; i < json.bookingitems.length; i++){
+            let gName = json.bookingitems[i].name;
+            let gEmail = json.bookingitems[i].email;
+            let gPAx = json.bookingitems[i].pax;
+            let gRemarks = json.bookingitems[i].pax;
+            let gId = json.bookingitems[i].id;
+
+            let row = bookingNameList.insertRow(bookingNameList.rows.length);
+            row.insertCell(0).innerHTML = gId;
+            row.insertCell(1).innerHTML = gName;
+            row.insertCell(2).innerHTML = gEmail;
+            row.insertCell(3).innerHTML = gPax;
+            row.insertCell(4).innerHTML = gRemarks;
+            row.insertCell(5).innerHTML = "TBC";
+        }
+     })
+}
