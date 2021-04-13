6f85064689dc226a430679dd1b2665d56ec110a2
Ng Chia Hui <ngchiahui95@gmail.com>
2021-04-12 19:36:15 -0700

    2027

--- tableapp/js/book.js
+++ tableapp/js/book.js
@@ -1,18 +1,36 @@
 let bookNowBtn = document.getElementById("bookNow");
 bookNowBtn.addEventListener("click",function(){
-let userName = document.getElementById("userName");
-let userNameVal = userName.value;
+    let userName = document.getElementById("userName");
+    let userNameVal = userName.value;
 
-let userEmail = document.getElementById("userEmail");
-let userEmailVal = userEmail.value;
+    let userEmail = document.getElementById("userEmail");
+    let userEmailVal = userEmail.value;
 
-let userPaxVal = document.getElementById("userPax").value;
-let userRemarksVal = document.getElementById("userRemarks").value;
+    let userPaxVal = document.getElementById("userPax").value;
+    let userRemarksVal = document.getElementById("userRemarks").value;
 
-BookNow(userNameVal, userEmailVal, userPaxVal, userRemarksVal);
+    BookNow(userNameVal, userEmailVal, userPaxVal, userRemarksVal);
 
 });
 
 function BookNow(userName, userEmail, userPax, userRemarks){
-    alert(userName);
+
+    let url = 'https://api.sheety.co/9d74311ba789d76dc962bf092a41ae29/tableapp/bookingitems';
+    let body = {
+      bookingitem: {
+        name: userName,
+        email: userEmail,
+        pax:userPax,
+        remarks:userRemarks
+      }
+    }
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(body)
+    })
+    .then((response) => response.json())
+    .then(json => {
+      // Do something with object
+     alert(json.bookingitem.name + " sucessfully added!"));
+    });
 }
