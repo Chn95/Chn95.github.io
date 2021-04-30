f40ee22953cc491cc9d7a3c4103b87d3b89e69ca
Ng Chia Hui <ngchiahui95@gmail.com>
2021-04-30 09:01:30 -0700

    0950

--- tableapp/js/book.js
+++ tableapp/js/book.js
@@ -37,20 +37,3 @@ function BookNow(userName, userEmail, userPax, userRemarks){
     });
 }
 
-function openMenu(evt, menuName) {
-
-  var i, tabcontent, tablinks;
-
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-
-  document.getElementById(menuName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
