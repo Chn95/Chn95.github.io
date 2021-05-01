f26371106708e9714e0fc3182b952c8cebcdb272
Ng Chia Hui <ngchiahui95@gmail.com>
2021-04-30 18:56:36 -0700

    1947

--- tableapp/js/delivery.js
+++ tableapp/js/delivery.js
@@ -1,18 +1,16 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-function openMenu(evt, menuName) {
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
 
-    var i, tabcontent, tablinks;
-  
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+    this.classList.toggle("active");
+
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
     }
-  
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-  
-    document.getElementById(menuName).style.display = "block";
-    evt.currentTarget.className += " active";
-  }
+  });
+}
